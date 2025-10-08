5e6405a94d6bcfe2b1070e4f6a3205d59d85f4c8
VINEETA KUMARI MEENA <viitameena117@gmail.com>
2025-10-09 04:19:34 +0530

    Define subscription schema with subscriber and channel fields

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,15 @@
 import mongoose , {Schema} from "mongoose";
 
-const subscriptionSchema = new Schema({},{timestamps:true})
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId , //who is subscribing
+        ref: "User",
+    },
+    channel:{
+        type: Schema.Types.ObjectId , // to which subscriber is subscribing
+        ref: "User"
+    }
+},{timestamps:true})
 
 
 export const Subscription = mongoose.model("Subscription" ,  subscriptionSchema);
